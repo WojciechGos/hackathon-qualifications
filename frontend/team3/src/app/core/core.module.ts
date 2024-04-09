b525aab8e235f9216daf0c003e8fa9c71ab4557a
Czechu7 <choocas0@gmail.com>
2024-04-09 18:41:10 +0200

    add navbar n footer

--- frontend/team3/src/app/core/core.module.ts
+++ frontend/team3/src/app/core/core.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NavbarComponent,
+    FooterComponent
+  ],
   imports: [SharedModule],
 })
 export class CoreModule {}
