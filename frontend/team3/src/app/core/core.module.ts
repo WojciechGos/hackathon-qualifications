70979665631b83a654c872b895ee4e7ef2ac5c0b
Michał Rusak <104869035+michalrusak@users.noreply.github.com>
2024-04-09 20:18:12 +0200

    finish merge

--- frontend/team3/src/app/core/core.module.ts
+++ frontend/team3/src/app/core/core.module.ts
@@ -3,26 +3,11 @@ import { SharedModule } from '../shared/shared.module';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    NavbarComponent,
-    FooterComponent
-  ],
-  exports: [
-    NavbarComponent,
-    FooterComponent
-  ],
-<<<<<<< HEAD
-  imports: [SharedModule],
-
-
-  
-=======
-  imports: [SharedModule, RouterModule]
-
->>>>>>> 0904Czech
+  declarations: [NavbarComponent, FooterComponent],
+  exports: [NavbarComponent, FooterComponent],
+  imports: [SharedModule, RouterModule, HttpClientModule],
 })
 export class CoreModule {}
-
-
